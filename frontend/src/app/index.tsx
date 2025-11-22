e00395cc1d5de639f1ce100e9553f2df5ec8e0a4
Hampus Bosson <hampus.bosson@outlook.com>
2025-11-22 12:39:29 +0100

    wrap app in authProvider

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,13 +1,16 @@
 import type React from "react";
 import AppRouter from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "@/context/AuthContext";
 
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <AppRouter />
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
     </QueryClientProvider>
   );
 };
